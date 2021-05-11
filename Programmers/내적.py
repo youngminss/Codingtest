c5f15cf9fb8cb358d35c238f245d75c00540c780
WI Young Min <youngminieo1005@gmail.com>
2021-05-11 23:03:15 +0900

    programmers 내적 solve

--- Programmers/내적.py
+++ Programmers/내적.py
@@ -4,6 +4,10 @@ def solution(a, b):
         answer += (a[i] * b[i])
     return answer
 
+if __name__ == '__main__':
+    result = solution([-1,3,2,-4],[2,2,6,7])
+    print(result)
+
 # [문제] 내적
 # [문제링크] https://programmers.co.kr/learn/courses/30/lessons/70128
 # [카테고리] 수학
