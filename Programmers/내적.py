950381cc1dd9a00407ea51f3d87532fc7d560920
WI Young Min <youngminieo1005@gmail.com>
2021-05-11 22:58:38 +0900

    programmers 내적 solve

---
+++ Programmers/내적.py
@@ -0,0 +1,11 @@
+def solution(a, b):
+    answer = 0
+    for i in range(len(a)):
+        answer += (a[i] * b[i])
+    return answer
+
+# [문제] 내적
+# [문제링크] https://programmers.co.kr/learn/courses/30/lessons/70128
+# [카테고리] 수학
+# [실행시간] O(n)
+
