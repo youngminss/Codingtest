dbacede78511f1987c16c03eeda0873eafa8135e
WI Young Min <youngminieo1005@gmail.com>
2021-05-15 23:16:19 +0900

    programmers 음양 더하기 solve

---
+++ Programmers/음양 더하기.py
@@ -0,0 +1,23 @@
+def solution(absolutes, signs):
+    answer = 0
+
+    for i in range(len(absolutes)):
+        if signs[i] :
+            answer += absolutes[i]
+        else :
+            answer -= absolutes[i]
+
+    return answer
+
+
+if __name__ == '__main__':
+    result = solution([4,7,12],[true,false,true])
+    print(result)
+
+# [4,7,12],[true,false,true]
+# [1,2,3],[false,false,true]
+
+# [문제] 음양 더하기 / 프로그래머스 월간 코드 챌린지 시즌 2
+# [문제링크] https://programmers.co.kr/learn/courses/30/lessons/76501
+# [카테고리] 구현
+# [실행시간] O(n)
