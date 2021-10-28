c27d30135ba9a7767ec83bb5067dc4cd30d72977
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2021-10-28 23:12:39 +0900

    Remind: 백준 기본문제 풀이 with Python(정렬)

---
+++ Baekjoon/10989.py
@@ -0,0 +1,12 @@
+import sys
+
+def sort_nums(array) :
+    array.sort()
+    [print(data) for data in array]
+
+if __name__ == '__main__' :
+    num = int(sys.stdin.readline().rstrip('\n'))
+    
+    array = []
+    for _ in range(num) : array.append(int(sys.stdin.readline().rstrip('\n')))
+    sort_nums(array) 
---
+++ Baekjoon/11652.py
@@ -0,0 +1,17 @@
+import sys
+
+def run_card_game(num) :
+    card_dict = {}
+    for _ in range(num) :
+        card = int(sys.stdin.readline().rstrip('\n'))
+        if card in card_dict :
+            card_dict[card] += 1
+        if not card in card_dict :
+            card_dict[card] = 1 
+    card_list = list(card_dict.items())
+    card_list.sort(key=lambda card : [-card[1], card[0]])
+    print(card_list[0][0])
+
+if __name__ == '__main__' :
+    num = int(sys.stdin.readline().rstrip('\n'))
+    run_card_game(num)
