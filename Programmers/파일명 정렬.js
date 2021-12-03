afa681590338aa1570386ac1c4e5f31f7b28240c
Wie Youngmin <50790145+youngminss@users.noreply.github.com>
2021-12-04 00:00:49 +0900

    Comment: 프로그래머스 - 파일명 정렬 주석

--- Programmers/파일명 정렬.js
+++ Programmers/파일명 정렬.js
@@ -57,7 +57,13 @@ solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.
 solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"]); // ["A-10 Thunderbolt II", "B-50 Superfortress", "F-5 Freedom Fighter", "F-14 Tomcat"]
 
 /**
- * 문제: 파일명 정렬
+ * 문제: 파일명 정렬 (2018 KAKAO BLIND - Level 2)
  * 테마: 구현, 문자열
  * 출처: https://programmers.co.kr/learn/courses/30/lessons/17686
+ *
+ * [Approach]
+ * + 정렬에 필요하다고 생각한 것 -> [head, number, file명 원본]
+ * + 1차적으로 head 즉, 각 item[0] 번째 원소로 비교를 실행
+ * + 2차적으로 number 즉, 각 item[1] 번째 원소로 비교를 실행
+ * + 그럼에도 두 비교값이 같다면 순서 그대로 진행
  */
