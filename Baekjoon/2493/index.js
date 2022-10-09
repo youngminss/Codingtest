dfdd21ce11dba020820253e1612c603195d559d6
youngmin wi <youngminieo1005@gmail.com>
2022-10-09 20:36:17 +0900

    solve: 백준 - 탑(2493)

---
+++ Baekjoon/2493/index.js
@@ -0,0 +1,36 @@
+const solution = (n, tops) => {
+  let result = [0];
+  let stack = [];
+
+  for (let i = 1; i < n; i++) {
+    if (tops[i] < tops[i - 1]) {
+      result.push(i);
+      stack.push([i - 1, tops[i - 1]]);
+      continue;
+    }
+
+    while (stack.length) {
+      const pop = stack[stack.length - 1];
+      if (tops[i] <= pop[1]) {
+        result.push(pop[0] + 1);
+        break;
+      }
+
+      stack.pop();
+    }
+
+    if (stack.length === 0) {
+      result.push(0);
+    }
+  }
+
+  console.log(result.join(" "));
+};
+
+const main = () => {
+  const path = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+  const [n, input] = require("fs").readFileSync(path).toString().trim().split("\n");
+  solution(Number(n), input.split(" ").map(Number));
+};
+
+main();
