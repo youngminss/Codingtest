2c97980396506ca5e98555bc83dbad1ed15df58b
youngmin wi <youngminieo1005@gmail.com>
2022-10-09 21:01:11 +0900

    comment: 알고리즘 작동 원리 주석 추가

--- Baekjoon/2493/index.js
+++ Baekjoon/2493/index.js
@@ -1,14 +1,16 @@
 const solution = (n, tops) => {
-  let result = [0];
-  let stack = [];
+  let result = [0]; // 가장 왼쪽 탑은 어차피 전파 받아줄 탑이 없으므로 0 으로 초기화
+  let stack = []; // 가장 가깝게 전파를 받을 수 있는 탑 정보 스택
 
   for (let i = 1; i < n; i++) {
+    // 현재 탑 기준, 바로 옆 탑이 전파를 받을 수 있으면 처리
     if (tops[i] < tops[i - 1]) {
       result.push(i);
       stack.push([i - 1, tops[i - 1]]);
       continue;
     }
 
+    // 현재 탑 기준, 바로 옆 탑이 전파를 받을 수 없으면, 스택에 있는 탑 정보중 가장 가까운 탑 정보를 뽇아내면서 전파를 받을 탑 정보 처리
     while (stack.length) {
       const pop = stack[stack.length - 1];
       if (tops[i] <= pop[1]) {
@@ -19,6 +21,7 @@ const solution = (n, tops) => {
       stack.pop();
     }
 
+    // 만약에, 스택에 존재하는 탑 정보를 다 뽑아냈음에도 현재 탑의 전파를 받을 탑이 없을 경우 처리
     if (stack.length === 0) {
       result.push(0);
     }
